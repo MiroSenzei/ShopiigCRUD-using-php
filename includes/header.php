e5b91209d902598475737f9e76363c55a81a9fac
Romer Jimenez <116239847+Reraizen@users.noreply.github.com>
2023-12-08 11:08:16 +0800

    second commit

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP CRUD MYSQL</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <!-- BOOTSTRAP 4 -->
+    <link rel="stylesheet" href="./boostrap/bootstrap.css">
+    <link rel="stylesheet" href="./boostrap/bootstrap.min.css">
+    <link rel="stylesheet" href="./boostrap/bootstrap.css">
+    <!-- FONT AWESOEM -->
+    <link rel="stylesheet" href="./font/all.css">
+    <link rel="stylesheet" href="./font/all.min.css">
+    <link rel="stylesheet" href="./font/fontawesome.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">CRUD SHOPPING LIST</a>
+      </div>
+    </nav>
